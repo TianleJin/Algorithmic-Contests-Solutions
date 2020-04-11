200c73531dc5d1726d7888613dda3c8d83f3b72f
TianleJin <tianlejin99@gmail.com>
2020-04-11 14:00:34 +1000

    My attempts

--- Google-Code-Jam-2020/Round-1A/a.py
+++ Google-Code-Jam-2020/Round-1A/a.py
@@ -6,18 +6,47 @@ from bisect import bisect_left, bisect_right
 from heapq import heapify, heappush, heappop, nlargest, nsmallest
 from itertools import permutations, combinations, zip_longest
 
-istr = lambda: input()
+
+istr = lambda: input().strip()
 istrl = lambda: input().strip().split()
 iint = lambda: int(input())
 iintl = lambda: list(map(int, input().strip().split()))
 
 
-def solve():
-    return 0
+def mergePrefix(prefixes):
+    ans = ""
+    for p in prefixes:
+        if len(p) > len(ans):
+            ans = p
+    for p in prefixes:
+        if ans.find(p) != 0:
+            return None
+    return ans
+
+
+def mergeSuffix(suffixes):
+    ans = mergePrefix([s[::-1] for s in suffixes])
+    if ans is None:
+        return ans
+    return ans[::-1]
+
+
+def solve(N, strs):
+    patterns = [s.split('*') for s in strs]
+    prefix = mergePrefix([p[0] for p in patterns])
+    suffix = mergeSuffix([p[-1] for p in patterns])
+    if prefix is None or suffix is None:
+        return '*'
+    ans = prefix
+    for p in patterns:
+        ans += ''.join(p[1:-1])
+    ans += suffix
+    return ans
 
 
 if __name__ == "__main__":
-    T = int(input())
+    T = iint()
     for t in range(1, T + 1):
-        ans = solve()
+        N = iint()
+        ans = solve(N, [istr() for _ in range(N)])
         print('Case #{}: {}'.format(t, ans))
--- Google-Code-Jam-2020/Round-1A/b.py
+++ Google-Code-Jam-2020/Round-1A/b.py
@@ -6,18 +6,45 @@ from bisect import bisect_left, bisect_right
 from heapq import heapify, heappush, heappop, nlargest, nsmallest
 from itertools import permutations, combinations, zip_longest
 
-istr = lambda: input()
+istr = lambda: input().strip()
 istrl = lambda: input().strip().split()
 iint = lambda: int(input())
 iintl = lambda: list(map(int, input().strip().split()))
 
 
-def solve():
-    return 0
+def solve(N):
+    print(1, 1)
+    if N == 1:
+        return
+
+    print(2, 1)
+    if N == 2:
+        return 
+
+    if N == 3:
+        print(2, 2)
+        return
+
+    N -= 2
+    j = 3
+    val = 2
+
+    while N >= val:
+        print(j, 2)
+        N -= val
+        val += 1
+        j += 1
+    
+    j -= 1
+    while N > 0:
+        print(j, 1)
+        N -= 1
+        j += 1
 
 
 if __name__ == "__main__":
     T = int(input())
     for t in range(1, T + 1):
-        ans = solve()
-        print('Case #{}: {}'.format(t, ans))
+        print("Case #{}:".format(t))
+        solve(iint())
+        
