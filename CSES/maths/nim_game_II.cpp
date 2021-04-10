b9cd91f671470e66ec32135d82ed7fe664c979c2
TianleJin <tianlejin99@gmail.com>
2021-04-10 21:59:11 +1000

    added three problems on nim

---
+++ CSES/maths/nim_game_II.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <array>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+const int dx4[] = { -1, 0, 1, 0 };
+const int dy4[] = { 0, -1, 0, 1 };
+const int dx8[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+const int dy8[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int T;
+	cin >> T;
+	while (T--) {
+		int n;
+		cin >> n;
+		int nim = 0;
+		for (int i = 0; i < n; i++) {
+			int pile;
+			cin >> pile;
+			nim ^= pile % 4;
+		}
+		if (nim == 0) {
+			cout << "second" << endl;
+		}
+		else {
+			cout << "first" << endl;
+		}
+	}
+}
