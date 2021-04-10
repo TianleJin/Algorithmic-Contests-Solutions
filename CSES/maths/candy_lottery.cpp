a140c197425e4f66ae8ccf2b92207947f307a138
TianleJin <tianlejin99@gmail.com>
2021-04-10 20:33:02 +1000

    added candy_lottery.cpp

---
+++ CSES/maths/candy_lottery.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <array>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+const int dx4[] = { -1, 0, 1, 0 };
+const int dy4[] = { 0, -1, 0, 1 };
+const int dx8[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+const int dy8[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, m;
+	cin >> n >> m;
+	vector<vector<double>> dp(n + 1, vector<double>(m + 1));
+	
+	dp[0][0] = 1;
+	for (int i = 0; i < n; i++) {
+		double pref = 0;
+		for (int j = 1; j <= m; j++) {
+			pref += dp[i][j - 1];
+            dp[i + 1][j] += pref / m;
+			dp[i + 1][j] += j * dp[i][j] / m;
+		}
+	}
+
+	double ans = 0;
+	for (int i = 1; i <= m; i++) {
+		ans += i * dp[n][i];
+	}
+	cout << setprecision(6) << fixed << ans << endl;
+}
