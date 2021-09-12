5d571f5b6c41cf0a9179c7406111d115bb4f23ce
TianleJin <tianlejin99@gmail.com>
2021-09-12 13:18:04 +1000

    Revised max flow problems.

--- CSES/graph_algorithms/distinct_routes.cpp
+++ CSES/graph_algorithms/distinct_routes.cpp
@@ -1,123 +1,88 @@
 #include <iostream>
-#include <string>
-#include <array>
-#include <vector>
 #include <algorithm>
-#include <sstream>
-#include <queue>
-#include <deque>
-#include <bitset>
-#include <iterator>
-#include <list>
-#include <stack>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <limits>
-#include <time.h>
-#include <math.h>
-#include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <assert.h>
-
+#include <vector>
+#include <queue>
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
 
-const int dx4[] = { -1, 0, 1, 0 };
-const int dy4[] = { 0, -1, 0, 1 };
-const int dx8[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
-const int dy8[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
-
-// https://cses.fi/problemset/task/1711/
-// max flow min cut
-const int mxn = 501;
-const int mxm = 2002;
-
+const int mxn = 500, mxm = 1000, inf = 2000000000;
+int n, m, par[mxn];
+bool vis[mxn];
+vector<int> path, graph[mxn];
 struct Edge {
-	int from, to, capacity, rev;
-};
+	int a, b, c, r;
+} edges[2 * mxm];
 
-int n, m;
-vector<int> graph[mxn];
-int par[mxn];
-bool vis[mxn], can[mxn];
-Edge edges[mxm];
-vector<int> ans;
-
-void dfs(int cur) {
-	ans.push_back(cur);
-	if (cur == n) return;
-	for (int i : graph[cur]) {
-		if (!edges[i].capacity && i % 2 == 0) {
-			edges[i].capacity = 1;
-			dfs(edges[i].to);
+void dfs(int u) {
+	path.push_back(u);
+	if (u == n - 1) return;
+	for (int i : graph[u]) {
+		if (i % 2 == 0 && edges[i].c == 0) {
+			edges[i].c = 1;
+			dfs(edges[i].b);
 			return;
 		}
 	}
 }
 
+// https://cses.fi/problemset/task/1711/
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+	ios::sync_with_stdio(false); cin.tie(0);
 	cin >> n >> m;
 	for (int i = 0; i < m; i++) {
-		int a, b;
-		cin >> a >> b;
-		int fwd = 2 * i, bwd = 2 * i + 1;
-		edges[fwd] = { a, b, 1, bwd };
-		edges[bwd] = { b, a, 0, fwd };
-		graph[a].push_back(fwd);
-		graph[b].push_back(bwd);
+		int a, b; cin >> a >> b; --a; --b;
+		int f = 2 * i, r = 2 * i + 1;
+		edges[f] = { a, b, 1, r };
+		edges[r] = { b, a, 0, f };
+		graph[a].push_back(f);
+		graph[b].push_back(r);
 	}
 
-	int mf = 0;
-	while (1) {
-		memset(vis, 0, sizeof(vis));
-		memset(par, 0, sizeof(par));
+	long long ans = 0;
+	while (true) {
 		bool found = false;
-		queue<int> q;
-		q.push(1);
-		vis[1] = true;
-		while (!q.empty() && !found) {
-			int cur = q.front(); q.pop();
-			for (int i : graph[cur]) {
+		queue<int> q; q.push(0);
+		memset(vis, 0, sizeof(vis)); vis[0] = true;
+		while (!found && q.size()) {
+			int a = q.front(); q.pop();
+			for (int i : graph[a]) {
 				Edge e = edges[i];
-				if (!vis[e.to] && e.capacity) {
-					vis[e.to] = 1;
-					par[e.to] = i;
-					q.push(e.to);
-					if (e.to == n) {
-						found = true;
-						break;
+				if (!vis[e.b] && e.c > 0) {
+					vis[e.b] = true;
+					par[e.b] = i;
+					q.push(e.b);
+					if (e.b == n - 1) {
+						found = true; break;
 					}
 				}
 			}
 		}
-		if (!found) {
-			break;
+		if (!found) break;
+		int mf = inf, u = n - 1;
+		while (u != 0) {
+			int f = par[u];
+			mf = min(mf, edges[f].c);
+			u = edges[f].a;
 		}
-		for (int cur = n; cur != 1; cur = edges[par[cur]].from) {
-			int i = par[cur];
-			edges[i].capacity = 0;
-			edges[edges[i].rev].capacity = 1;
-		}
-		mf++;
-	}
 
-	cout << mf << endl;
-	for (int i = 0; i < mf; i++) {
-		dfs(1);
-		cout << ans.size() << endl;
-		for (int x : ans) {
-			cout << x << ' ';
+		ans += mf;
+		u = n - 1;
+		while (u != 0) {
+			int f = par[u], r = edges[f].r;
+			edges[f].c -= mf;
+			edges[r].c += mf;
+			u = edges[f].a;
+		}
+	}
+	
+	cout << ans << endl;
+	for (int i = 0; i < ans; i++) {
+		dfs(0);
+		cout << (int)path.size() << endl;
+		for (int i = 0; i < (int)path.size(); i++) {
+			cout << path[i] + 1 << ' ';
 		}
 		cout << endl;
-		ans.clear();
+		path.clear();
 	}
 }
--- CSES/graph_algorithms/download_speed.cpp
+++ CSES/graph_algorithms/download_speed.cpp
@@ -1,116 +1,65 @@
 #include <iostream>
-#include <string>
-#include <array>
-#include <vector>
 #include <algorithm>
-#include <sstream>
-#include <queue>
-#include <deque>
-#include <bitset>
-#include <iterator>
-#include <list>
-#include <stack>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <limits>
-#include <time.h>
-#include <math.h>
-#include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <assert.h>
-
+#include <vector>
+#include <queue>
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
 
-const int dx4[] = { -1, 0, 1, 0 };
-const int dy4[] = { 0, -1, 0, 1 };
-const int dx8[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
-const int dy8[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
-const int dxknight[] = { -1, -1, -2, -2, 1, 1, 2, 2 };
-const int dyknight[] = { 2, -2, 1, -1, 2, -2, 1, -1 };
-
-// https://cses.fi/problemset/task/1694/
-// Ford Fulkerson Algorithm
-struct Edge {
-	int u, v, rev, cap;
-};
-
-const int mxn = 501;
-const int mxe = 1001;
-const ll inf = 1e15;
-int n, m;
-Edge edges[mxe << 1];
-vector<int> graph[mxn];
-int par[mxn];
+const int mxn = 500, mxm = 1000, inf = 2000000000;
+int n, m, par[mxn];
 bool vis[mxn];
+vector<int> graph[mxn];
+struct Edge {
+	int a, b, c, r;
+} edges[2 * mxm];
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+	ios::sync_with_stdio(false); cin.tie(0);
 	cin >> n >> m;
 	for (int i = 0; i < m; i++) {
-		int u, v, c;
-		cin >> u >> v >> c;
-		int fwd = 2 * i;
-		int rev = 2 * i + 1;
-		edges[fwd] = { u, v, c, rev };
-		edges[rev] = { v, u, 0, fwd };
-		graph[u].push_back(fwd);
-		graph[v].push_back(rev);
+		int a, b, c; cin >> a >> b >> c; --a; --b;
+		int f = 2 * i, r = 2 * i + 1;
+		edges[f] = { a, b, c, r };
+		edges[r] = { b, a, 0, f };
+		graph[a].push_back(f);
+		graph[b].push_back(r);
 	}
 
-	ll mf = 0;
-	const int src = 1;
-	const int snk = n;
-	while (1) {
-		queue<int> q;
-		q.push(src);
-		memset(vis, false, sizeof(vis));
-		vis[src] = true;
+	long long ans = 0;
+	while (true) {
 		bool found = false;
-		while (!q.empty() && !found) {
-			int u = q.front(); q.pop();
-			for (int i : graph[u]) {
+		queue<int> q; q.push(0);
+		memset(vis, 0, sizeof(vis)); vis[0] = true;
+		while (!found && q.size()) {
+			int a = q.front(); q.pop();
+			for (int i : graph[a]) {
 				Edge e = edges[i];
-				if (!vis[e.v] && e.cap > 0) {
-					vis[e.v] = true;
-					par[e.v] = i;
-					q.push(e.v);
-					if (e.v == snk) {
-						found = true;
-						break;
+				if (!vis[e.b] && e.c > 0) {
+					vis[e.b] = true;
+					par[e.b] = i;
+					q.push(e.b);
+					if (e.b == n - 1) {
+						found = true; break;
 					}
 				}
 			}
 		}
-
-		if (!vis[snk]) {
-			break;
+		if (!found) break;
+		int mf = inf, u = n - 1;
+		while (u != 0) {
+			int f = par[u];
+			mf = min(mf, edges[f].c);
+			u = edges[f].a;
 		}
 
-		ll pf = inf;
-		int v = snk;
-		while (v != src) {
-			int i = par[v];
-			pf = min(pf, (ll)edges[i].cap);
-			v = edges[i].u;
-		}
-
-		mf += pf;
-		v = snk;
-		while (v != src) {
-			int i = par[v];
-			edges[i].cap -= pf;
-			edges[edges[i].rev].cap += pf;
-			v = edges[i].u;
+		ans += mf;
+		u = n - 1;
+		while (u != 0) {
+			int f = par[u], r = edges[f].r;
+			edges[f].c -= mf;
+			edges[r].c += mf;
+			u = edges[f].a;
 		}
 	}
-	cout << mf << endl;
+	cout << ans << endl;
 }
